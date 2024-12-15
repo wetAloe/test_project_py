b765a6fa12c2c23f4648d836cf542fb3253421ea
Oleg Grigorev <oleggrigoryev22@gmail.com>
2024-12-15 13:56:54 +0200

    sad

--- scripts/upload_data.py
+++ scripts/upload_data.py
@@ -3,20 +3,28 @@ import csv
 
 import aiohttp
 from tqdm import tqdm
+from aiohttp import ClientSession
 
 
-async def upload_data():
-    async with aiohttp.ClientSession() as session:
-        with open("data/images.csv", "r") as file:
-            reader = csv.reader(file)
-            for i, row in tqdm(enumerate(reader)):
+# Function to read the CSV file and process rows asynchronously
+async def upload_data(file_path):
+    async with ClientSession() as session:
+        # Open the CSV file
+        with open(file_path, mode='r') as file:
+            csv_reader = csv.reader(file)
+
+            tasks = []
+            # Create a list to store tasks
+            for i, row in tqdm(enumerate(csv_reader)):
                 if i == 0 or row[0] == "":
                     continue
 
                 data = list(map(int, row[1:]))
-                await session.post("http://localhost:8000/images/", json={"data": data})
-                break
+                tasks.append(session.post("http://localhost:8000/images/", json={"data": data}))
+
+            # Use asyncio.gather to run all tasks concurrently
+            await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    asyncio.run(upload_data())
+    asyncio.run(upload_data("data/images.csv"))
